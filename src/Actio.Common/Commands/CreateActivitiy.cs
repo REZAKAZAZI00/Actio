cd7407c1e387bbc8cdd180ba4b505a161b7f3530
rezakazazy8 <rezakazazy8@yahoo.com>
2024-03-14 16:19:38 +0330

    Create CareteActivivtiy

---
+++ src/Actio.Common/Commands/CreateActivitiy.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Actio.Common.Commands
+{
+    public class CreateActivitiy : IAuthenticatedCommand
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+
+        public string Category { get; set; }
+        public string Name{ get; set; }
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
